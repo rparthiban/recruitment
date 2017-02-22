97d281280091c28fea8f99f3866b8eafb9294df9
Parthiban <parthiban@maasjuju.maasjuju.com>
2017-02-22 18:14:09 +0530

    missed out forms, now added

---
+++ application/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from models import Application
+
+class ApplicationForm(forms.ModelForm):
+
+	class Meta:
+		model = Application
+		fields = ('email', 'first_name', 'last_name', 'address', 'current_company', 'skills')
