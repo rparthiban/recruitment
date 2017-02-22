1fe631982abf92df5dc18c7b566a029f86ef415a
Parthiban <parthiban@maasjuju.maasjuju.com>
2017-02-22 11:47:03 +0530

    application fields defined

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import Application
+
+
+class ApplicationAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['email', 'first_name']}),
+        ('Information', {'fields': ['last_name', 'address',
+        	'current_company', 'skills', 'active', 'accepted']}),
+    ]
+    list_display = ('email', 'first_name', 'last_name',
+    	'current_company', 'skills', 'active', 'accepted')
+    list_filter = ['active', 'skills']
+    search_fields = ['email', 'first_name', 'current_company', 'skills']
+
+admin.site.register(Application, ApplicationAdmin)
--- application/models.py
+++ application/models.py
@@ -2,4 +2,25 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Application(models.Model):
+    """
+    Need fields to be defined for the this table with their attributes.
+    """
+
+    email = models.EmailField("Email", max_length=254, blank=False)
+    first_name = models.CharField("First Name", max_length=32, blank=False)
+    last_name = models.CharField("Last Name", max_length=32, blank=True)
+    address = models.TextField("Address", blank=True)
+    current_company = models.CharField("Current Company", max_length=100, blank=True)
+    skills = models.TextField("Skills", blank=False)
+    submitted = models.DateField(db_index=True, auto_now_add=True)
+    active = models.BooleanField(default=True)
+    accepted = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return '%s' % self.email
+
+    class Meta:
+        ordering = ['-submitted']
